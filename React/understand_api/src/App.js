5068e9adc317b8e7e97a6bac9f50b3813aaa223d
MiKL5 <miklgaillard@gmail.com>
2023-01-18 17:02:09 +0100

    WIP understand  api

---
+++ React/understand_api/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function App() {
+  const [array, setArray] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const click = () => {
+    axios
+      .get("http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query=" + search)
+      .then((response) => {
+        setArray(response.data.result)
+      })
+  }
+
+console.log(array);
+  return (
+    
+        array.map((film) =>  (
+          <>
+          <div>
+            {/* nom du film trouvé */}
+            {film.title}
+            <br />
+            {/* nom de l'image précédée du chemin pour afficher la jaquette */}
+            <img src={"https://image.tmdb.org/t/p/w185" + film.poster_path} />
+          );
+          </div>
+    </>
+        ) ) )
+        } 
